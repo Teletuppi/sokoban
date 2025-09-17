460b670d6f725d3ffee7b83f895568eac00f46d2
Eelis Askolin <gr301843@gradia.fi>
2025-09-17 14:57:32 +0300

    jee

--- skriptit/pelaaja.gd
+++ skriptit/pelaaja.gd
@@ -1,12 +1,9 @@
 extends RigidBody2D
 
 @export var speed := 200.0
-<<<<<<< HEAD
-=======
 
 func _ready() -> void:
 	lock_rotation = true
->>>>>>> 5339a5d54e1695c782736a9b9c2531ab857caf63
 
 func _physics_process(_delta: float) -> void:
 	var input_vector := Vector2.ZERO
