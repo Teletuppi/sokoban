01fe748e83425b7613495fbf2eb57fc9e24cc48b
Eelis Askolin <gr301843@gradia.fi>
2025-09-17 14:40:37 +0300

    jeeeee pelaaja rigidbody

--- skriptit/pelaaja.gd
+++ skriptit/pelaaja.gd
@@ -1,14 +1,14 @@
-extends CharacterBody2D
+extends RigidBody2D
 
-@export var speed := 200
+@export var speed := 200.0
 
 func _physics_process(_delta: float) -> void:
 	var input_vector := Vector2.ZERO
-
+	
 	input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	input_vector.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 
 	input_vector = input_vector.normalized()
-
-	velocity = input_vector * speed
-	move_and_slide()
+	
+	# Directly setting linear_velocity overrides physics forces (good for player control)
+	linear_velocity = input_vector * speed
