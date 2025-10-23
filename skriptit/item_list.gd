bce83918f65f00ad95a7f0592371894db8668f4d
matulehti <matiaslehtinen08@gmail.com>
2025-10-23 13:18:57 +0300

    a

--- skriptit/item_list.gd
+++ skriptit/item_list.gd
@@ -1,7 +1,11 @@
 extends ItemList
 
 func levelit_listaan() -> void:
-	
+	var laskin = 0
+	#var new_level = load(path).instantiate()
+	#leveli.add_child(new_level)
+	var path = "res://levels/Level_+" + laskin + ".tscn"
+	print("MEGA PATH YHISTYS " + path)
 	pass
 
 func _ready() -> void:
