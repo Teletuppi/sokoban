fbaeea4182aa7fb09d1ebf7ea1f4083c1a634c29
Pedgux <matiaslehtinen08@gmail.com>
2026-08-14 10:14:22 +0300

    Fixed errors from migrating from 4.5->4.7

--- skriptit/taso_johtaja.gd
+++ skriptit/taso_johtaja.gd
@@ -1,19 +1,22 @@
 extends Node
-@onready var current_level = load("res://levels/Level_0.tscn").instantiate()
+
+@onready var current_level = load("res://levels/level_0.tscn")
 @onready var leveli: Node = $leveli
 @onready var kamera: Camera2D = $Camera2D
 
 var olemassa = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	load_level("res://levels/Level_0.tscn")
+	if current_level == null:
+		print("ERROR: Initial level not found")
+		return
+	current_level = current_level.instantiate()
+	leveli.add_child(current_level)
 
 func load_signal_level(idx, _position, _mousebuttonn):
 	if _mousebuttonn == MOUSE_BUTTON_LEFT:
-		var path = "res://levels/Level_" + str(idx-1) + ".tscn"
+		var path = "res://levels/level_" + str(idx) + ".tscn"
 		load_level(path)
-		
-
 
 
 func load_level(path: String):
@@ -22,24 +25,28 @@ func load_level(path: String):
 	while is_instance_valid(current_level):
 		await get_tree().process_frame
 		print("GAGAGAGA")
-	var new_level = load(path).instantiate()
-	leveli.add_child(new_level)	
-	current_level = new_level
-	
+	var new_level = load(path)
+	if new_level == null:
+		print("ERROR: Level resource not found at " + path)
+		return
+	current_level = new_level.instantiate()
+	leveli.add_child(current_level)
+
 func _process(float) -> void:
 	if Input.is_action_just_pressed("esci"):
 		avaa_sulje_pause("res://scenes/pause_menu.tscn")
-	
 
 
 func avaa_sulje_pause(path: String):
 	var UI: Resource = load(path)
+	if UI == null:
+		print("ERROR: UI resource not found at " + path)
+		return
 	if olemassa == false:
 		var siistiUI: Control = UI.instantiate()
 		get_node("UI").add_child(siistiUI)
 		olemassa = true
 		siistiUI.get_node("ItemList").item_clicked.connect(load_signal_level.bind())
-		
 	else:
 		get_node("UI/PauseMenu").queue_free()
 		olemassa = false
