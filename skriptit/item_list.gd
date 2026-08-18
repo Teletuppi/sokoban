f1382606eca9e024b6468c6f85a3a750e51ccfca
Pedgux <matiaslehtinen08@gmail.com>
2026-08-18 12:24:33 +0300

    Viba fixit leveli loadauksee

--- skriptit/item_list.gd
+++ skriptit/item_list.gd
@@ -1,19 +1,30 @@
 extends ItemList
 
 func levelit_listaan() -> void:
-	var laskin = 0
-	var path = "res://levels/level_" + str(laskin) + ".tscn"
-	while ResourceLoader.exists(path):
-		var level = "level_" + str(laskin)
-		path = "res://levels/" + level + ".tscn"
-		laskin += 1
-		if ResourceLoader.exists(path):
-			add_item(level)
-	#var new_level = load(path).instantiate()
-	#leveli.add_child(new_level)
-	
-	
-	pass
+	var dir = DirAccess.open("res://levels/")
+	if dir == null:
+		print("ERROR: couldn't open res://levels/")
+		return
+
+	dir.list_dir_begin()
+	var file_name = dir.get_next()
+	var found: Array[String] = []
+	while file_name != "":
+		if not dir.current_is_dir() and file_name.begins_with("level_") and file_name.ends_with(".tscn"):
+			var level_name = file_name.get_basename()
+			var number_part = level_name.trim_prefix("level_")
+			if number_part.is_valid_int():
+				found.append(level_name)
+		file_name = dir.get_next()
+	dir.list_dir_end()
+
+	found.sort_custom(func(a, b):
+		return int(a.trim_prefix("level_")) < int(b.trim_prefix("level_"))
+	)
+
+	for level in found:
+		var idx = add_item(level)
+		set_item_metadata(idx, "res://levels/" + level + ".tscn")
 
 func _ready() -> void:
 	levelit_listaan()
--- skriptit/taso_johtaja.gd
+++ skriptit/taso_johtaja.gd
@@ -5,7 +5,8 @@ extends Node
 @onready var kamera: Camera2D = $Camera2D
 
 var olemassa = false
-# Called when the node enters the scene tree for the first time.
+var current_item_list: ItemList
+
 func _ready() -> void:
 	if current_level == null:
 		print("ERROR: Initial level not found")
@@ -15,16 +16,16 @@ func _ready() -> void:
 
 func load_signal_level(idx, _position, _mousebuttonn):
 	if _mousebuttonn == MOUSE_BUTTON_LEFT:
-		var path = "res://levels/level_" + str(idx) + ".tscn"
+		var path = current_item_list.get_item_metadata(idx)
+		if path == null:
+			print("ERROR: no metadata for item ", idx)
+			return
 		load_level(path)
 
-
 func load_level(path: String):
-	#print("ladattiin leveilili! CURRENT LEVEL ON: " + str(current_level))
 	current_level.queue_free()
 	while is_instance_valid(current_level):
 		await get_tree().process_frame
-		#print("GAGAGAGA")
 	var new_level = load(path)
 	if new_level == null:
 		print("ERROR: Level resource not found at " + path)
@@ -32,10 +33,9 @@ func load_level(path: String):
 	current_level = new_level.instantiate()
 	leveli.add_child(current_level)
 
-func _process(float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("esci"):
 		avaa_sulje_pause("res://scenes/pause_menu.tscn")
-
 
 func avaa_sulje_pause(path: String):
 	var UI: Resource = load(path)
@@ -46,7 +46,9 @@ func avaa_sulje_pause(path: String):
 		var siistiUI: Control = UI.instantiate()
 		get_node("UI").add_child(siistiUI)
 		olemassa = true
-		siistiUI.get_node("ItemList").item_clicked.connect(load_signal_level.bind())
+		current_item_list = siistiUI.get_node("ItemList")
+		current_item_list.item_clicked.connect(load_signal_level)
 	else:
 		get_node("UI/PauseMenu").queue_free()
+		current_item_list = null
 		olemassa = false
